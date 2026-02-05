01bea685f03431a84bc92d59197c7c07d0bc50b3
Tom Bailey <tom.bailey@optimummedical.co.uk>
2026-02-05 20:10:47 +0000

    Tidy up chapter 12

--- lib/chapter12/main.dart
+++ lib/chapter12/main.dart
@@ -1,42 +1,169 @@
 import 'package:flutter/material.dart';
-import 'package:get_it/get_it.dart';
-import 'package:hello_world/chapter12/football_ground_details_widget.dart';
-import 'package:hello_world/chapter12/football_ground_list_widget.dart';
-import 'package:hello_world/chapter12/football_ground_ratings.dart';
-import 'package:provider/provider.dart';
+import 'package:hello_world/Chapter12/animated_builder.dart';
+import 'package:hello_world/Chapter12/rotated_bounce_in_button.dart';
+import 'package:hello_world/Chapter12/rotation_button.dart';
+import 'package:hello_world/Chapter12/composed_button.dart';
+import 'package:hello_world/Chapter12/rotated_button.dart';
+import 'package:hello_world/Chapter12/scaled_button.dart';
+import 'package:hello_world/Chapter12/translated_button.dart';
+import 'package:hello_world/chapter12/move_button.dart';
+import 'package:hello_world/chapter12/scale_button.dart';
 
 void main() {
-  GetIt.instance.registerSingleton<FootballGroundRatingsProvider>(FootballGroundRatingsProvider());
-
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Animations',
+      theme: ThemeData(
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.blue, // Button background color
+            foregroundColor: Colors.white, // Text color
+          ),
+        ),
+      ),
+      home: const MyHomePage(title: 'Flutter Animations'),
+    );
+  }
 }
 
-class _MyAppState extends State<MyApp> {
-  String? _selectedFootballGround;
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
 
-  void _setFootballGround(String destinationfootballGroundName) {
-    this.setState(() {
-      _selectedFootballGround = destinationfootballGroundName;
-    });
-  }
+  final String title;
 
   @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider<FootballGroundRatingsProvider>(
-      create: (context) => GetIt.instance.get<FootballGroundRatingsProvider>(),
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          spacing: 10,
+          children: <Widget>[
+            ElevatedButton(
+              child: Text('Rotated button'),
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return RotatedButton();
+                    },
+                  ),
+                );
+              },
+            ),
+            ElevatedButton(
+              child: Text('Scaled button'),
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return ScaledButton();
+                    },
+                  ),
+                );
+              },
+            ),
+            ElevatedButton(
+              child: Text('Translated button'),
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return TranslatedButton();
+                    },
+                  ),
+                );
+              },
+            ),
+            ElevatedButton(
+              child: Text('Composed button'),
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return ComposedButton();
+                    },
+                  ),
+                );
+              },
+            ),
+            ElevatedButton(
+              child: Text('Animated rotation button'),
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return Scaffold(appBar: AppBar(), body: RotationAnimations());
+                    },
+                  ),
+                );
+              },
+            ),
+            ElevatedButton(
+              child: Text('Animated scale button'),
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return Scaffold(appBar: AppBar(), body: ScaleAnimations());
+                    },
+                  ),
+                );
+              },
+            ),
+            ElevatedButton(
+              child: Text('Animated moving button'),
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return Scaffold(appBar: AppBar(), body: MoveAnimations());
+                    },
+                  ),
+                );
+              },
+            ),
+            ElevatedButton(
+              child: Text('Animated bounce-in button'),
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return Scaffold(appBar: AppBar(), body: RotationBounceInAnimations());
+                    },
+                  ),
+                );
+              },
+            ),
+            ElevatedButton(
+              child: Text('Animation builder'),
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return Scaffold(appBar: AppBar(), body: RotationBuilderAnimations());
+                    },
+                  ),
+                );
+              },
+            ),
+          ],
         ),
-        home: FootballGroundListWidget(),
       ),
     );
   }
